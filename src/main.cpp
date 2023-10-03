37d4ce12f50bb3368a455d9775cf2757503cee02
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-10-03 12:25:07 +0200

    feat: parse args in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 #define cimg_display 0
-#include "CImg.h"
-#include "ImageProc/Image.h"
 #include <iostream>
+
+#include "CImg.h"
+
+#include "ImageProc/Image.h"
+#include "CLI/InputParser.hpp"
+#include "config.hpp"
+
 using namespace cimg_library;
-int main()
+
+int main(int argc, char **argv)
 {
-    std::cout << hello() << std::endl;
-    CImg<unsigned char> image("lenac.bmp"); // create the image from a file (must exist in the working dir)
-    for (int x = 0; x < image.width(); x++) {
-        for (int y = 0; y < image.height() / 2; y++) { // only upper half of the image gets processed
-            float valR = image(x, y, 0); // Read red value at coordinates (x, y)
-            float valG = image(x, y, 1); // Read green value at coordinates (x, y)
-            float valB = image(x, y, 2); // Read blue value at coordinates (x, y)
-            float avg = (valR + valG + valB) / 3; // Compute average pixel value (grey)
-            image(x, y, 0) = avg;
-            image(x, y, 1) = avg;
-            image(x, y, 2) = avg;
-        }
+    cli::InputParser input(argc, argv);
+    if(input.cmdOptionExists("--help")){
+        std::cout << config::programUsageInfo<< std::endl;
     }
-    image.save_bmp("out.bmp"); // save the modified image to a file
+    const std::string &filename = input.getCmdOption("-f");
+    if (!filename.empty()){
+        
+    }
     return 0;
 }
