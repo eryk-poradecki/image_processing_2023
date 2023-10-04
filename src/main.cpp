ce07d191b5cdd9f22ec6d7ecf4c65d6ae9393082
eryk-poradecki <eryk.poradecki@gmail.com>
2023-10-04 09:12:09 +0200

    refactor: data copying logic put into a function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "CLI/InputParser.hpp"
-#include "ImageProc/Types.h" // Include the Image header
+#include "ImageProc/Types.h"
 #include "config.hpp"
 #include "ImageProc/ElementaryOperations.h"
 
 using namespace cimg_library;
+
+void copyImageDataBack(CImg<unsigned char>& cimgImage, const Image& imageArray);
 
 int main(int argc, char** argv)
 {
@@ -19,10 +21,8 @@ int main(int argc, char** argv)
         int height = image.height();
         int spectrum = image.spectrum();
 
-        // Create the Image object
         Image imageArray(height, std::vector<unsigned char>(width * spectrum));
 
-        // Copy data into the Image object
         for (int i = 0; i < height; ++i) {
             for (int j = 0; j < width * spectrum; ++j) {
                 imageArray[i][j] = image.data()[i * width * spectrum + j];
@@ -33,40 +33,34 @@ int main(int argc, char** argv)
             float factor = std::stof(input.getCmdOption("--brightness"));
             elementary::adjustBrightness(imageArray, width, height, spectrum, factor);
 
-            // Copy data back to the CImg object
-            for (int i = 0; i < height; ++i) {
-                for (int j = 0; j < width * spectrum; ++j) {
-                    image.data()[i * width * spectrum + j] = imageArray[i][j];
-                }
-            }
-
+            copyImageDataBack(image, imageArray);
             image.save("output.bmp");
         }
         else if (input.cmdOptionExists("--contrast")) {
             float factor = std::stof(input.getCmdOption("--contrast"));
             elementary::adjustContrast(imageArray, width, height, spectrum, factor);
 
-            // Copy data back to the CImg object
-            for (int i = 0; i < height; ++i) {
-                for (int j = 0; j < width * spectrum; ++j) {
-                    image.data()[i * width * spectrum + j] = imageArray[i][j];
-                }
-            }
-
+            copyImageDataBack(image, imageArray);
             image.save("output.bmp");
         }
         else if (input.cmdOptionExists("--negative")) {
             elementary::createNegative(imageArray, width, height, spectrum);
 
-            // Copy data back to the CImg object
-            for (int i = 0; i < height; ++i) {
-                for (int j = 0; j < width * spectrum; ++j) {
-                    image.data()[i * width * spectrum + j] = imageArray[i][j];
-                }
-            }
-
+            copyImageDataBack(image, imageArray);
             image.save("output.bmp");
         }
     }
     return 0;
+}
+
+void copyImageDataBack(CImg<unsigned char>& cimgImage, const Image& imageArray) {
+    int width = cimgImage.width();
+    int height = cimgImage.height();
+    int spectrum = cimgImage.spectrum();
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width * spectrum; ++j) {
+            cimgImage.data()[i * width * spectrum + j] = imageArray[i][j];
+        }
+    }
 }
