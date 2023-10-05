cb0177f2a4aa09aa57af1426be5b0d562c7526ff
eryk-poradecki <eryk.poradecki@gmail.com>
2023-10-05 14:39:16 +0200

    feat: added a test case for adjusting contrast

--- tests/elementary_operations_test.cpp
+++ tests/elementary_operations_test.cpp
@@ -114,8 +114,43 @@ TEST(ElementaryOperations, AdjustPositiveConstrastWithDiffSpectrumTest)
 
 TEST(ElementaryOperations, AdjustContrastTest)
 {
-    // TODO
+    imgVec inputVec(3, std::vector<unsigned char>(3 * 2));
+    inputVec[0] = { 255, 0, 0, 255, 255, 0 };
+    inputVec[1] = { 1, 128, 128, 64, 64, 5 };
+    inputVec[2] = { 4, 5, 6, 1, 2, 3 };
+    Image img = Image(inputVec, 3);
+
+    float contrastFactor = 1.5; 
+
+    imgVec expectedVec(3, std::vector<unsigned char>(inputVec[0].size()));
+        for (auto& row : expectedVec) {
+            for (auto& pixel : row) {
+                unsigned char inputPixel = inputVec[&row - &expectedVec[0]][&pixel - &row[0]];
+
+                float expectedFactor = (259.0f * (contrastFactor + 255.0f)) / (255.0f * (259.0f - contrastFactor));
+
+                int newRed = static_cast<int>(expectedFactor * (static_cast<float>(inputPixel) - 128.0f) + 128.0f);
+                int newGreen = static_cast<int>(expectedFactor * (static_cast<float>(inputPixel) - 128.0f) + 128.0f);
+                int newBlue = static_cast<int>(expectedFactor * (static_cast<float>(inputPixel) - 128.0f) + 128.0f);
+
+                newRed = std::max(0, std::min(255, newRed));
+                newGreen = std::max(0, std::min(255, newGreen));
+                newBlue = std::max(0, std::min(255, newBlue));
+
+                pixel = static_cast<unsigned char>(newRed);
+                pixel = static_cast<unsigned char>(newGreen);
+                pixel = static_cast<unsigned char>(newBlue);
+            }
+        }
+
+
+    elementary::adjustContrast(img, contrastFactor);
+
+    EXPECT_TRUE(areVectorsEqual(expectedVec, img.getImgVec()));
 }
+
+
+
 
 TEST(ElementaryOperations, CreateNegativeTest)
 {
