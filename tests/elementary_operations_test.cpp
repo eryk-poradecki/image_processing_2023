2191a4ba598a0855bdf3819f8b9b82e7e7e1ea16
eryk-poradecki <eryk.poradecki@gmail.com>
2023-10-05 14:50:09 +0200

    feat: added testing for spectrum 1 vector

--- tests/elementary_operations_test.cpp
+++ tests/elementary_operations_test.cpp
@@ -114,6 +114,8 @@ TEST(ElementaryOperations, AdjustPositiveConstrastWithDiffSpectrumTest)
 
 TEST(ElementaryOperations, AdjustContrastTest)
 {
+    //=================================================
+    // spectrum 3
     imgVec inputVec(3, std::vector<unsigned char>(3 * 2));
     inputVec[0] = { 255, 0, 0, 255, 255, 0 };
     inputVec[1] = { 1, 128, 128, 64, 64, 5 };
@@ -147,6 +149,32 @@ TEST(ElementaryOperations, AdjustContrastTest)
     elementary::adjustContrast(img, contrastFactor);
 
     EXPECT_TRUE(areVectorsEqual(expectedVec, img.getImgVec()));
+
+    //=================================================
+    // spectrum 1
+    imgVec inputVec1(3, std::vector<unsigned char>(3 * 2));
+    inputVec1[0] = { 255, 0, 0, 255, 255, 0 };
+    inputVec1[1] = { 1, 128, 128, 64, 64, 5 };
+    inputVec1[2] = { 4, 5, 6, 1, 2, 3 };
+    Image img1 = Image(inputVec1, 1);
+
+    imgVec expectedVec1(3, std::vector<unsigned char>(inputVec1[0].size()));
+    for (auto& row : expectedVec1) {
+        for (auto& pixel : row) {
+            unsigned char inputPixel = inputVec1[&row - &expectedVec1[0]][&pixel - &row[0]];
+
+            float expectedFactor = (259.0f * (contrastFactor + 255.0f)) / (255.0f * (259.0f - contrastFactor));
+
+            int newPixel = static_cast<int>(expectedFactor * (static_cast<float>(inputPixel) - 128.0f) + 128.0f);
+            newPixel = std::max(0, std::min(255, newPixel));
+
+            pixel = static_cast<unsigned char>(newPixel);
+        }
+    }
+
+    elementary::adjustContrast(img1, contrastFactor);
+
+    EXPECT_TRUE(areVectorsEqual(expectedVec1, img1.getImgVec()));
 }
 
 
