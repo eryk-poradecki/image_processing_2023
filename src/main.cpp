ac03909f14120b0576dff303f65be80d8f63b9c7
eryk-poradecki <eryk.poradecki@gmail.com>
2023-10-07 12:48:49 +0200

    feat: added CImg to rgb converting

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "CImg.h"
 #include "CLI/InputParser.hpp"
 #include "ImageProc/ElementaryOperations.h"
+#include "ImageProc/GeometricOperations.h"
 #include "ImageProc/Types.h"
 #include "config.hpp"
 #include "CImg.h"
@@ -10,7 +11,20 @@ using namespace cimg_library;
 using namespace ImageProc;
 
 // declaration
-void copyImageDataBack(CImg<unsigned char>& cimgImage, const imgVec& imageArray);
+imgVec convertCimgtoImageVector(const CImg<unsigned char>& image);
+
+void convertToCimgAndCopyBack(CImg<unsigned char>& cimgImage, const imgVec& imageArray) {
+    // convert ImageVector (std::vector<std::vector<Pixel>>) to CImg image and save
+
+    // copy data from imageVector to CImg
+    for (int i = 0; i < imageArray.size(); i++) {
+        for (int j = 0; j < imageArray[0].size(); j++) {
+            cimgImage(i, j, 0, 0) = imageArray[i][j].r;
+            cimgImage(i, j, 0, 1) = imageArray[i][j].g;
+            cimgImage(i, j, 0, 2) = imageArray[i][j].b;
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -25,46 +39,61 @@ int main(int argc, char** argv)
         int height = image.height();
         int spectrum = image.spectrum();
 
-        imgVec imageArray(height, std::vector<unsigned char>(width * spectrum));
-
-        for (int i = 0; i < height; ++i) {
-            for (int j = 0; j < width * spectrum; ++j) {
-                imageArray[i][j] = image.data()[i * width * spectrum + j];
-            }
-        }
+        imgVec imageArray = convertCimgtoImageVector(image);
 
         Image img = Image(imageArray, spectrum);
 
         if (input.cmdOptionExists("--brightness")) {
             int factor = std::stof(input.getCmdOption("--brightness"));
             elementary::adjustBrightness(img, factor);
-            copyImageDataBack(image, img.getImgVec());
-            image.save("output.bmp");
+            convertToCimgAndCopyBack(image, img.getImgVec());
         }
         if (input.cmdOptionExists("--contrast")) {
             float factor = std::stof(input.getCmdOption("--contrast"));
             elementary::adjustContrast(img, factor);
-            copyImageDataBack(image, img.getImgVec());
-            image.save("output.bmp");
+            convertToCimgAndCopyBack(image, img.getImgVec());
         }
         if (input.cmdOptionExists("--negative")) {
             elementary::createNegative(img);
-            copyImageDataBack(image, img.getImgVec());
-            image.save("output.bmp");
+            convertToCimgAndCopyBack(image, img.getImgVec());
         }
+        if (input.cmdOptionExists("--vflip")) {
+            geometric::verticalFlip(img);
+            convertToCimgAndCopyBack(image, img.getImgVec());
+        }
+        if (input.cmdOptionExists("--hflip")) {
+            geometric::horizontalFlip(img);
+            convertToCimgAndCopyBack(image, img.getImgVec());
+        }
+        image.save("output.bmp");
+
+
     }
     return 0;
 }
 
-void copyImageDataBack(CImg<unsigned char>& cimgImage, const imgVec& imageArray)
+imgVec convertCimgtoImageVector(const CImg<unsigned char>& image)
 {
-    int width = cimgImage.width();
-    int height = cimgImage.height();
-    int spectrum = cimgImage.spectrum();
+    int width = image.width();
+    int height = image.height();
+    int spectrum = image.spectrum();
+    int pixel_count = width * height;
 
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width * spectrum; ++j) {
-            cimgImage.data()[i * width * spectrum + j] = imageArray[i][j];
+    imgVec imageArray;
+    std::vector<Pixel> row;
+
+    for (int i = 0; i < height; i++) {
+        row.clear();
+        for (int j = 0; j < width; j++) {
+            Pixel pix;
+            // https://stackoverflow.com/questions/3291923/how-to-get-rgb-value-by-cimg
+            pix.r = image(i, j, 0, 0);
+            pix.g = image(i, j, 0, 1);
+            pix.b = image(i, j, 0, 2);
+            row.push_back(pix);
         }
+        imageArray.push_back(row);
     }
+
+    return imageArray;
 }
