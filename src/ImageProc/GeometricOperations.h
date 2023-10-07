b4de98fcb697ea14cb299848a6de904db4ad4441
eryk-poradecki <eryk.poradecki@gmail.com>
2023-10-07 12:47:30 +0200

    feat: added vertical and horizontal flip functions

---
+++ src/ImageProc/GeometricOperations.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Types.h"
+
+namespace ImageProc {
+
+    namespace geometric {
+
+    void verticalFlip(Image& image);
+
+    void horizontalFlip(Image& image);
+
+} // namespace geometric
+} // namespace ImageProc
