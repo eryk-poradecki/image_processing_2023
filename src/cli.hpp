a4a0544a08010e8d79b55ac3e01dc9e36715ca93
rostekus <rostyslavmosorov@gmail.com>
2023-10-17 18:26:25 +0200

    feat: added options to cli

--- src/cli.hpp
+++ src/cli.hpp
@@ -4,6 +4,7 @@
 #include "ImageProc/ElementaryOperations.h"
 #include "ImageProc/GeometricOperations.h"
 #include "ImageProc/NoiseRemoval.h"
+#include "ImageProc/ImgAnalysis.h"
 #include "ImageProc/Types.h"
 #include "config.hpp"
 #include "files.hpp"
@@ -79,7 +80,28 @@ inline void cliMain(int argc, char** argv)
             int minFilterSize = std::stoi(input.getCmdOption("--min"));
             int maxFilterSize = std::stoi(input.getCmdOption("--max"));
             imgVec filteredImageVec = noise::adaptiveMedianFilter(img, minFilterSize, maxFilterSize);
-
+        	Image outImg = Image(filteredImageVec);
+			if(input.cmdOptionExists("--mse")){
+			
+				std::cout << "MSE:" << analysis::calculateMSE(img, outImg) << "\n";
+			}
+			
+			if(input.cmdOptionExists("--pmse")){
+			
+				std::cout << "PMSE:" << analysis::calculatePMSE(img, outImg) << "\n";
+			}
+			if(input.cmdOptionExists("--psnr")){
+			
+				std::cout << "PSNR:" << analysis::calculatePSNR(img, outImg) << "\n";
+			}
+			if(input.cmdOptionExists("--md")){
+			
+				std::cout << "MD:" << analysis::calculateMD(img, outImg) << "\n";
+			}
+			if(input.cmdOptionExists("--snr")){
+			
+				std::cout << "SNR:" << analysis::calculatSNR(img, outImg) << "\n";
+			}
             CImg<unsigned char> cimgFilteredImage(filteredImageVec.size(), filteredImageVec[0].size(), 1, filteredImageVec[0][0].size(), 0);
 
             convertToCimgAndCopyBack(cimgFilteredImage, filteredImageVec);
