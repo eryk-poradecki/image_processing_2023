63499ad801f424d00ae153791bc125ec2703b308
rostekus <rostyslavmosorov@gmail.com>
2023-10-18 12:50:56 +0200

    refactor: splitted histogram functions

--- src/ImageProc/Histogram.cpp
+++ src/ImageProc/Histogram.cpp
@@ -1,22 +1,14 @@
 #include "Histogram.h"
+#include "ImageProc/Types.h"
+#include <vector>
 
 using namespace ImageProc;
 void histogram::createAndSaveHist(ImageProc::Image& img, std::string filename, size_t nBins)
 {
-    auto imgVec = img.getImgVec();
     int numOfHist = img.getSpectrum();
     int col = img.getWidth();
     int row = img.getHeight();
-    std::cout << numOfHist << std::endl;
-    std::vector<std::vector<unsigned char>> splitRGBVec(numOfHist, std::vector<unsigned char>(row * col, 150));
-    for (size_t i = 0; i < col; i++) {
-        for (size_t j = 0; j < row; j++) {
-            for (size_t k = 0; k < numOfHist; k++) {
-                splitRGBVec[k][i * col + j] = imgVec[i][j][k];
-            }
-        }
-    }
-
+    auto splitRGBVec = histogram::splitRGBImgToSaperateLayerRGB(img);
     for (int i = 0; i < numOfHist; i++) {
         matplot::subplot(1, numOfHist, i);
         matplot::hist(splitRGBVec[i], nBins);
@@ -26,4 +18,27 @@ void histogram::createAndSaveHist(ImageProc::Image& img, std::string filename, s
         ax->x_axis().label_font_size(12);
     }
     matplot::save(filename);
+}
+
+imgVec histogram::createHistorgram(ImageProc::Image& img, size_t nBins)
+{
+    auto splitVec = splitRGBImgToSaperateLayerRGB(img);
+    // decide what is histogram
+}
+
+std::vector<std::vector<unsigned char>> histogram::splitRGBImgToSaperateLayerRGB(ImageProc::Image& img)
+{
+    int numOfHist = img.getSpectrum();
+    int col = img.getWidth();
+    int row = img.getHeight();
+    std::vector<std::vector<unsigned char>> splitRGBVec(numOfHist, std::vector<unsigned char>(row * col, 0));
+    auto imgMatrix = img.getImgVec();
+    for (size_t i = 0; i < col; i++) {
+        for (size_t j = 0; j < row; j++) {
+            for (size_t k = 0; k < numOfHist; k++) {
+                splitRGBVec[k][i * col + j] = imgMatrix[i][j][k];
+            }
+        }
+    }
+    return splitRGBVec;
 }
--- src/ImageProc/Histogram.h
+++ src/ImageProc/Histogram.h
@@ -9,5 +9,8 @@
 namespace ImageProc::histogram{
 	constexpr size_t NUM_BINS = 255; 
 
-	void createAndSaveHist(ImageProc::Image& img, std::string filename, size_t nBins = NUM_BINS);
-}
+	void createAndSaveHist(ImageProc::Image& img, std::string filename, size_t nBins = NUM_BINS);	
+
+	std::vector<std::vector<unsigned char>> splitRGBImgToSaperateLayerRGB(ImageProc::Image& img);
+
+	imgVec createHistorgram(ImageProc::Image& img, size_t nBins);}
