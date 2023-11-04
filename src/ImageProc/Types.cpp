633180776dd0e4e138ac3dec8aba690b47e3c665
rostekus <rostyslavmosorov@gmail.com>
2023-11-04 14:23:12 +0100

    feat: added move operator for image

--- src/ImageProc/Types.cpp
+++ src/ImageProc/Types.cpp
@@ -27,12 +27,30 @@ int Image::getSpectrum() const noexcept
     return mSpectrum;
 }
 
-bool Image::operator!=(const Image& other) const
+Image::Image()
+    : mWidth(0)
+    , mHeight(0)
+    , mSpectrum(0)
 {
-    return this->getImgVec() != other.getImgVec();
+    mImgVec = imgVec {};
 }
-
-bool Image::operator==(const Image& other) const
+Image& Image::operator=(Image&& other) noexcept
 {
-    return this->getImgVec() == other.getImgVec();
+    if (this != &other) {
+        // Release current resources
+        mImgVec.clear(); // Assuming the ownership is within mImgVec
+
+        // Move the data and control variables
+        mImgVec = std::move(other.mImgVec);
+        mWidth = other.mWidth;
+        mHeight = other.mHeight;
+        mSpectrum = other.mSpectrum;
+
+        // Reset the source object
+        other.mWidth = 0;
+        other.mHeight = 0;
+        other.mSpectrum = 0;
+        other.mImgVec.clear();
+    }
+    return *this;
 }
