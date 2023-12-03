7bd930f247b692cdfb81608ad2c6587c540a993b
eryk-poradecki <eryk.poradecki@gmail.com>
2023-12-03 13:30:34 +0100

    feat: add morph opening, closing and M1 operation

--- src/ImageProc/MorphologicalOperations.h
+++ src/ImageProc/MorphologicalOperations.h
@@ -5,4 +5,12 @@ namespace ImageProc::morph {
 ImageProc::imgVec dilation(ImageProc::Image& img);
 
 ImageProc::imgVec erosion(ImageProc::Image& img);
+
+ImageProc::imgVec opening(ImageProc::Image& img);
+
+ImageProc::imgVec closing(ImageProc::Image& img);
+
+ImageProc::imgVec elementwiseDivision(const imgVec& img1, const imgVec& img2);
+
+ImageProc::imgVec operationM1(ImageProc::Image& img);
 }
