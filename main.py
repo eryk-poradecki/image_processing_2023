4cc8a8fc086dafbcfa8629237c8494cf813bafe2
rostekus <rostyslavmosorov@gmail.com>
2024-01-19 19:15:13 +0100

    feat: just main

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import numpy as np
+
 from src.argparser import ConsoleParser
+from src.fourier_transform import fft_2d, inv_fft_2d
 from src.image import ImageReader, ImageWriter
-if __name__ == '__main__':
+from src.visualize import save_fourier_spectrum, visualize_fourier_spectrum
+
+
+if __name__ == "__main__":
     parser = ConsoleParser()
-    args = parser.parse_args()
-    print(args.filename)
-    img = ImageReader.read(args.filename)
-    ImageWriter.write('output.png', img)
+    # args = parser.parse_args()
+    # print(args.filename)
+    img = ImageReader.read("lenag.bmp")
+    ImageWriter.write("output.png", img)
+
+    fft = inv_fft_2d(img)
+    inv = fft_2d(fft)
+    # visualize_fourier_spectrum(fft)
+    # visualize_fourier_spectrum(img)
+    save_fourier_spectrum(fft)
