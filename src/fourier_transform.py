971665977bedaf9041c1fff984d13e405c88b9e4
rostekus <rostyslavmosorov@gmail.com>
2024-01-19 14:52:37 +0100

    feat: fft2d and inv

--- src/fourier_transform.py
+++ src/fourier_transform.py
@@ -31,3 +31,19 @@ def invfft(x: np.ndarray) -> np.ndarray:
     second_half = even + t[N // 2 :] * odd
 
     return np.concatenate([first_half, second_half]) / 2
+
+
+def fft_2d(X: np.ndarray) -> np.ndarray:
+    x_fft_rows = np.apply_along_axis(fft, axis=1, arr=X)
+
+    x_fft_2d = np.apply_along_axis(fft, axis=0, arr=x_fft_rows)
+
+    return x_fft_2d
+
+
+def invfft_2d(X):
+    x_invfft_cols = np.apply_along_axis(invfft, axis=0, arr=X)
+
+    x_invfft_2d = np.apply_along_axis(invfft, axis=1, arr=x_invfft_cols)
+
+    return x_invfft_2d
