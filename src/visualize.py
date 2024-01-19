76e1b8c11eca7e3851733af493805becf28e658b
rostekus <rostyslavmosorov@gmail.com>
2024-01-19 19:14:50 +0100

    feat: visualize and save func for fft

---
+++ src/visualize.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualize_fourier_spectrum(fft_2d):
+    fft_shifted = np.fft.fftshift(fft_2d)
+
+    magnitude_spectrum = np.abs(fft_shifted)
+
+    plt.imshow(np.log1p(magnitude_spectrum), cmap="viridis")
+    plt.colorbar()
+    plt.title("Fourier Spectrum")
+    plt.show()
+
+
+def save_fourier_spectrum(fft_2d):
+    fft_shifted = np.fft.fftshift(fft_2d)
+
+    magnitude_spectrum = np.abs(fft_shifted)
+
+    plt.imshow(np.log1p(magnitude_spectrum), cmap="viridis", origin="lower")
+    plt.colorbar()
+    plt.title("Fourier Spectrum")
+    plt.savefig("fourier.png")
