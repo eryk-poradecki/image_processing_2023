4af68bd9322f918030f561bf2255f5aa6f559baa
eryk-poradecki <eryk.poradecki@gmail.com>
2024-01-21 15:23:49 +0100

    feat: add argparser handling for each filter

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import numpy as np
 
 from src.argparser import ConsoleParser
-from src.fourier_transform import fft_2d, inv_fft_2d
 from src.image import ImageReader, ImageWriter
-from src.visualize import save_fourier_spectrum, visualize_fourier_spectrum
-
+from src.filters import low_pass_filter, high_pass_filter, band_pass_filter, band_cut_filter, apply_filter, high_pass_filter_edge, phase_modifying_filter
+from src.numpy_filters import low_pass_filter_np, high_pass_filter_np, band_pass_filter_np, band_cut_filter_np, phase_modifying_filter_np
 
 if __name__ == "__main__":
     parser = ConsoleParser()
-    # args = parser.parse_args()
-    # print(args.filename)
-    img = ImageReader.read("lenag.bmp")
-    ImageWriter.write("output.png", img)
+    args = parser.parse_args()
 
-    fft = inv_fft_2d(img)
-    inv = fft_2d(fft)
-    # visualize_fourier_spectrum(fft)
-    # visualize_fourier_spectrum(img)
-    save_fourier_spectrum(fft)
+    img = ImageReader.read(args.filename)
+    mask = ImageReader.read(args.mask) if args.mask else None
+
+    if args.filter == 'low_pass':
+        result = apply_filter(low_pass_filter, img, band_size=args.band)
+    elif args.filter == 'high_pass':
+        result = apply_filter(high_pass_filter, img, band_size=args.band)
+    elif args.filter == 'band_pass':
+        result = apply_filter(band_pass_filter, img, band_size_low=args.bandlow, band_size_high=args.bandhigh)
+    elif args.filter == 'band_cut':
+        result = apply_filter(band_cut_filter, img, band_size_low=args.bandlow, band_size_high=args.bandhigh)
+    elif args.filter == 'high_pass_edge':
+        result = apply_filter(high_pass_filter_edge, img, mask)
+    elif args.filter == 'phase_modifying':
+        result = apply_filter(phase_modifying_filter, img, l=args.l, k=args.k)
+    else:
+        raise ValueError(f'Unsupported filter type: {args.filter}')
+    
+    output_path = f"output_{args.filter}.png"
+    
+    ImageWriter.write(output_path, result.astype(np.uint8))
+
