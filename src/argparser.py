f441182346944761fc7b9c886d7a31a064363b68
eryk-poradecki <eryk.poradecki@gmail.com>
2024-01-22 11:40:34 +0100

    feat: add visualize argument

--- src/argparser.py
+++ src/argparser.py
@@ -9,7 +9,7 @@ class ConsoleParser:
         self.parser.add_argument('-f', '--filename', type=str, help='Specify the file name')
         self.parser.add_argument('--mask', type=str, help='Specify the mask file name')
         self.parser.add_argument('--filter', choices=['low_pass', 'high_pass', 'band_pass', 'band_cut', 'high_pass_edge', 'phase_modifying'], help='Specify the filter type')
-
+        self.parser.add_argument('-v', '--visualize', action='store_true', help='Specify if FFT should be visualized')
         self.parser.add_argument('--band', type=int, help='Specify the band size for filters')
         self.parser.add_argument('--bandlow', type=int, help='Specify the low band size for band-pass and band-cut filters')
         self.parser.add_argument('--bandhigh', type=int, help='Specify the high band size for band-pass and band-cut filters')
